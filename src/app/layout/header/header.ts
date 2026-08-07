25e0399c4cb335a2055d8638e4ecf0e68ea44b43
DaKo1312 <korbmacher.d@outlook.de>
2026-08-07 11:43:36 +0200

    fix: header hover effect

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -10,6 +10,7 @@ export type Language = 'EN' | 'DE';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
+  protected readonly activeLink = signal<string | null>(null);
   protected readonly languages: readonly Language[] = ['EN', 'DE'];
   protected readonly language = signal<Language>('EN');
   protected selectLanguage(language: Language): void {
